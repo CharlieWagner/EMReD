ae7e141726a625ae76ff9be4fc476e8ec754d1c9
Charlie Wagner <c.wagner@student.isartdigital.com>
2020-02-10 20:21:19 +0100

    Zone 01 Prototyping

---
+++ Assets/Scripts/LD/Boulders.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boulders : MonoBehaviour
+{
+
+    public Rigidbody[] _Boulders;
+    public GameObject _GroundCollider;
+    private bool _HasFallen;
+
+    private void Start()
+    {
+        _SetBouldersKinematic(true);
+
+        _GroundCollider.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        
+        if (other.tag == "Player")
+        {
+            _SetBouldersKinematic(false);
+            _GroundCollider.SetActive(false);
+            Debug.Log("boom");
+        }
+    }
+
+
+    private void _SetBouldersKinematic(bool status = false)
+    {
+        for (int i = 0; i <= _Boulders.Length - 1; i++)
+        {
+            _Boulders[i].isKinematic = status;
+        }
+    }
+}
