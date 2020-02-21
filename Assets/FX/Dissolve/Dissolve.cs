82c66a4739ae58f82b47eca95ff27ad3080fa08c
Charlie Wagner <c.wagner@student.isartdigital.com>
2020-02-21 15:30:45 +0100

    Les cailloux brûlent
    le post proc était cassé

---
+++ Assets/FX/Dissolve/Dissolve.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    [SerializeField]
+    [Range(-.1f,1f)]
+    private float _dissolveAmount;
+
+    private Rigidbody RB;
+    private Animator Anim;
+    private Material Mat;
+
+    private void Start()
+    {
+        Mat = GetComponent<Renderer>().material;
+        RB = GetComponent<Rigidbody>();
+        Anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Mat.SetFloat("_Visi", _dissolveAmount);
+
+        if (!RB.isKinematic)
+        {
+            Anim.SetBool("Boom", true);
+        }
+    }
+
+    public void Kill()
+    {
+        Destroy(gameObject);
+    }
+}
