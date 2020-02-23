ce881ef352d11f354b3d8f2ed1645aba761e78fa
Charlie Wagner <c.wagner@student.isartdigital.com>
2020-02-23 17:48:22 +0100

    End Animation - WIP

---
+++ Assets/Scripts/LD/CoreController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreController : MonoBehaviour
+{
+    private Renderer _R;
+    private Material _CoreMat;
+
+    [SerializeField]
+    [Range(-1f,1f)]
+    private float _Strength = -.06f;
+
+    [SerializeField]
+    private rotate[] _RotationHubs;
+
+    private void Start()
+    {
+        _R = transform.GetChild(0).GetComponent<Renderer>();
+        _CoreMat = _R.material;
+    }
+
+    private void Update()
+    {
+        _CoreMat.SetFloat("_DisplaceStr", _Strength);
+    }
+
+    public void initRotate()
+    {
+        for (int i = 0; i < _RotationHubs.Length; i++)
+        {
+            _RotationHubs[i]._StartRotation = true;
+        }
+
+    }
+}
--- Assets/Scripts/LD/rotate.cs
+++ Assets/Scripts/LD/rotate.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class rotate : MonoBehaviour
 {
-    public float rotationSpeed;
+    [SerializeField]
+    private bool _RotateByDefault = false;
+    [SerializeField]
+    private float _rotationSpeed;
+
+
+    public bool _StartRotation = false;
 
     void Update()
     {
-        transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0));
+        if (_RotateByDefault)
+        {
+            transform.Rotate(new Vector3(0, _rotationSpeed * Time.deltaTime, 0));
+        } else if (_StartRotation)
+        {
+            transform.Rotate(new Vector3(0, _rotationSpeed * Time.deltaTime, 0));
+        }
+
+        
     }
 }
