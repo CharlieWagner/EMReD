8045047186a7e8c979bfea50043ac43956281b21
Charlie Wagner <c.wagner@student.isartdigital.com>
2020-03-11 20:27:02 +0100

    Lab Improvements + door & elevator

---
+++ Assets/Scripts/Misc/Elevator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+
+    private GameObject _Player;
+
+    private Rigidbody _PlayerRB;
+
+    [SerializeField]
+    private float _forceStr;
+
+    private void Start()
+    {
+        _Player = GameObject.FindWithTag("Player");
+        _PlayerRB = _Player.GetComponent<Rigidbody>();
+    }
+    
+
+    // Update is called once per frame
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _PlayerRB.AddForce(new Vector3(0,-_forceStr,0), ForceMode.Acceleration);
+        }
+    }
+}
