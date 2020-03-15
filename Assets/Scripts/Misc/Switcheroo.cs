dcf683ad03e39407fd8b9db443df2ad058bb487c
Charlie Wagner <c.wagner@student.isartdigital.com>
2020-03-15 16:13:48 +0100

    Custom Sky

---
+++ Assets/Scripts/Misc/Switcheroo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switcheroo : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _FakeObelisk;
+
+    [SerializeField]
+    private GameObject _RealObelisk;
+
+    private void Start()
+    {
+        _FakeObelisk.SetActive(true);
+        _RealObelisk.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("switch");
+        _FakeObelisk.SetActive(false);
+        _RealObelisk.SetActive(true);
+    }
+}
