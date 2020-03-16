0da77688215464e01147fc69930e380bacb9f618
Charlie Wagner <c.wagner@student.isartdigital.com>
2020-03-16 16:48:12 +0100

    VA implementation start

---
+++ Assets/Scripts/Sound/VoiceLineTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceLineTrigger : MonoBehaviour
+{
+    public AudioSource Source;
+    private void Start()
+    {
+        Source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Source.Play();
+        }
+    }
+}
