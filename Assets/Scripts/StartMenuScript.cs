ed04e029dca1aba4e395590df71bc5f9507f7582
Charlie Wagner <c.wagner@student.isartdigital.com>
2020-03-27 19:04:43 +0100

    Start menu tweaks

---
+++ Assets/Scripts/StartMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private Scene MainScene;
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            SceneManager.LoadScene(MainScene.name);
+        }
+    }
+}
