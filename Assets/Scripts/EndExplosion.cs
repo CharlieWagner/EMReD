60d425c01e2654b9fd2bf951254ffa6852a851f7
Charlie Wagner <c.wagner@student.isartdigital.com>
2020-03-27 23:10:59 +0100

    Fix stuff

---
+++ Assets/Scripts/EndExplosion.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class EndExplosion : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _AnimCtrl;
+    [SerializeField]
+    private AudioSource _audioSource;
+    [SerializeField]
+    private AudioMixer mainMixer;
+    [SerializeField]
+    private Transform _BloomBall;
+
+    [SerializeField]
+    private string _CreditsScene;
+
+    private bool _goBoom = false;
+    private GameObject Player;
+
+    private bool _CanSelfDestruct = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player = other.gameObject;
+            _CanSelfDestruct = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (_CanSelfDestruct && Input.GetButton("SelfDestruct"))
+        {
+            _AnimCtrl.SetTrigger("boom");
+            _goBoom = true;
+            float volume;
+            mainMixer.GetFloat("volume", out volume);
+            _audioSource.volume = volume;
+        }
+
+        if (_goBoom)
+        {
+            _BloomBall.transform.position = Player.transform.position;
+        }
+
+    }
+
+    public void _Playsource()
+    {
+        _audioSource.Play();
+    }
+
+    public void _End()
+    {
+        SceneManager.LoadScene(_CreditsScene);
+    }
+}
---
+++ Assets/Scripts/Sound/PlaySound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaySound : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource Source;
+    [SerializeField]
+    private AudioSource Source2;
+
+    [SerializeField]
+    private string SceneToLoad;
+
+    [SerializeField]
+    private GameObject EndPrompt;
+
+    public void PlaySoundEvent()
+    {
+        Source.Play();
+    }
+
+    public void PlaySoundEvent2()
+    {
+        Source2.Play();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && EndPrompt.activeSelf)
+        {
+            SceneManager.LoadScene(SceneToLoad);
+        }
+    }
+
+}
